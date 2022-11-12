abdebdc1fd568b01f138aba4841e06d944b58177
Raul Ochoa <raulochoa039@gmail.com>
2022-11-11 21:44:54 -0600

    Change port

--- app.js
+++ app.js
@@ -185,6 +185,6 @@ app.get ("/:customListName", (req, res) => {
     });
 });
 
-app.listen (process.env.PORT || 4000, () => {
+app.listen (4000, () => {
     console.log ("Server running on port 4000.");
 });
